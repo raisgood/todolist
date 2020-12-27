7b27fa6a95454c67d41e7cd87565f1097cdedf18
raisgood <rashadthecoder@outlook.com>
2020-12-27 08:25:26 -0600

    todolist initial commit

---
+++ todolist-app/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm/TodoForm';
+import TodoList from './components/TodoList/TodoList';
+
+function App() {
+    const [counter, setCounter] = useState(4);
+    const [data, setData] = useState([
+        { todolist: 'What Do You Need To Do?', time: 'Choose a Time', key: 0 },
+        { todolist: 'What Do You Need To Do?', time: 'Choose a Time', key: 2 },
+        { todolist: 'What Do You Need To Do?', time: 'Choose a Time', key: 3 },
+        { todolist: 'What Do You Need To Do?', time: 'Choose a Time', key: 4 },
+    ]);
+
+    const handleClear = (event) => {
+        event.preventDefault();
+        setData([{ todolist: 'What Do You Need To Do?', time: 'Choose a Time', key: '' }]);
+    }
+
+    const handleAdd = (todolist, time) => {
+        const key = counter;
+        setCounter(counter + 1);
+        setData(oldData => [...oldData, { todolist, time, key }]);
+    }
+
+    const handleUpdate = (key, todolist, time) => {
+        setData(oldData => oldData.map(item => {
+            if (key === item.key) {
+                return {
+                    todolist,
+                    time,
+                    key
+                };
+            } else {
+                return item;
+            }
+        }));
+    }
+
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        h2 > To Do List < /h2>  <
+        /header>  <
+        TodoForm handleClear = { handleClear }
+        handleAdd = { handleAdd }
+        />  <
+        TodoList items = { data }
+        handleUpdate = { handleUpdate }
+        />  <
+        /div>
+    );
+}
+
+export default App;
---
+++ todolist-app/src/components/TodoForm/TodoForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './TodoForm.css';
+
+export default function TodoForm(props) {
+    const updateData = event => {
+        event.preventDefault();
+        const todolist = document.querySelector('.js-todolist-item').value;
+        const time = document.querySelector('.js-time').value;
+        document.querySelector('.js-todolist-item').value = '';
+        document.querySelector('.js-time').value = '';
+        props.handleAdd(todolist, time);
+    }
+
+  return (
+    <form onSubmit={ updateData }>
+      <input type="text" name="todolist-item" className="js-todolist-item" placeholder="Enter Activity" />
+      <input type="text" className="js-time" placeholder="Enter Time" />
+    <button type="submit">Add Activity</button>
+    <button onClick={props.handleClear}>Clear Activity</button>
+    </form>
+  )
+}
