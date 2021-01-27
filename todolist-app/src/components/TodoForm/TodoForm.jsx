3390a3b59512d06b6989e3049743bf40b5b41095
Rashad Humphrey <rashadthecoder@gmail.com>
2021-01-26 18:02:24 -0600

    update

--- todolist-app/src/components/TodoForm/TodoForm.jsx
+++ todolist-app/src/components/TodoForm/TodoForm.jsx
@@ -15,10 +15,10 @@ export default function TodoForm(props) {
 
   return (
     <form onSubmit={ updateData }>
-    <label className="label1"><strong>List Your "To Do" List Activity Here:</strong></label>
-      <input type="text" ClassName="todolist-item" className="js-todolist-item" placeholder="List Activity" />
-      <label className="label2"><strong>List the Time and "Press Add":</strong></label>
-      <input type="text" className="js-time" placeholder="List Time" />
+    <label className="label1"><strong>Write your Scheduled Activity Here:</strong></label>
+      <input type="text" ClassName="todolist-item" required className="js-todolist-item" placeholder="List Activity" />
+      <label className="label2"><strong>List the Scheduled Time and "Press Add":</strong></label>
+      <input type="text" className="js-time" required placeholder="List Time" />
     <button type="submit">Add</button>
     <button onClick={props.handleClear}>Clear</button>
     </form>
